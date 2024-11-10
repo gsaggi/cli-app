6c6191f76fcb7133c4dc6f8e6a4681bba6dbc2ec
Gregory Saggi <gregory.saggi4@studytafensw.edu.au>
2024-11-11 08:34:43 +1100

    add server API for managing products and categories with routes, models, and database configuration

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { fetchCategories } from './utils/api';
+import { useEffect, useState } from 'react';
 export default function App() {
+  
+  const [categories, setCategories] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const d = await fetchCategories();
+        setCategories(d);
+      } catch (error) { }
+    };
+    fetchData();
+  },[])
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {
+        categories.map((category) => (
+          <Text key={category.id}>{category.name}</Text>
+        ))
+      }
       <StatusBar style="auto" />
     </View>
   );
